e364279b8e54248b44b20adc6130746924ed4006
abhi d <abhijeetd439@gmail.com>
2025-03-20 13:22:20 +0530

    new file to perfome CURD opration in this

---
+++ app.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const userModel = require('./moduls/user');
+
+app.set('view engine', 'ejs');
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+    res.render("index");
+});
+
+app.post('/create', async (req, res) => {
+    let { name, email, imageurl } = req.body;
+    let createUser = await userModel.create({ name, email, imageurl });
+    res.redirect('/read');
+});
+
+app.get('/read', async (req, res) => {
+    let users = await userModel.find();
+    res.render("read", { users });
+});
+
+
+app.get("/delete/:id", async (req, res) => {
+    let users = await userModel.findOneAndDelete({ _id: req.params.id });
+    res.redirect('/read');
+});
+
+app.get("/edit/:id", async (req, res) => {
+    let user = await userModel.findOne({ _id: req.params.id });
+    res.render("edit", { user });
+});
+app.post("/update/:id", async (req, res) => {
+    let { name, email, imageurl } = req.body;
+    let user = await userModel.findOneAndUpdate({ _id: req.params.id }, { name, email, imageurl } , { new: true });
+    res.redirect("/read");
+});
+
+
+
+app.listen(3000, () => {
+    console.log('Example app listening on port 3000!');
+});
